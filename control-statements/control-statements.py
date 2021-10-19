b423822a0996980236242654145b2a44fa1993b0
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-10-19 23:44:23 +0530

    completed control statements in python

---
+++ control-statements/control-statements.py
@@ -0,0 +1,21 @@
+# Make use of colons and indentation (white space)
+
+temp = 18
+
+# if
+if temp == 18:
+    print("18")
+
+# if/else
+if temp != 18:
+    print("Not 18")
+else:
+    print("18")
+
+# if/elif/else
+if temp < 18:
+    print("Less than 18")
+elif temp == 18:
+    print("Equal to 18")
+else:
+    print("Greater than 18")
