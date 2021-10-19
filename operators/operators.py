02c8f3cecabdfa9a3d5358579768219d7d893c57
Bhagya Mudgal <bhagyamudgal@gmail.com>
2021-10-19 23:21:14 +0530

    completed operators in python

---
+++ operators/operators.py
@@ -0,0 +1,14 @@
+# < > <= >= == != 
+print(10 > 5)
+print("Bhagya"=="bhagya")
+print("Bhagya"=="Bhagya")
+
+# and
+print(10 < 5 and 10 > 5)
+
+# or
+print(10 < 5 or 10 > 5)
+
+# not
+print(not 10>5)
+print(not 10<5)
